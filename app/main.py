16928dc0f29d575f89eb4588e41c1551fd8dc6cc
LASC-dev <lucas.a.s.casas@outlook.es>
2026-03-19 20:32:38 -0300

    test: add value and type test
    feat: add validation for age type

--- app/main.py
+++ app/main.py
@@ -18,6 +18,8 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
         get_human_age(15, 15) == [1, 1]
         get_human_age(24, 24) == [2, 2]
     """
+    if not isinstance(cat_age, int) or not isinstance(dog_age, int):
+        raise TypeError("Age must be a natural number")
     if cat_age < 0 or dog_age < 0:
         raise ValueError("Age cannot be negative")
 
--- app/test_main.py
+++ app/test_main.py
@@ -20,3 +20,31 @@ def test_get_human_age_conversion(
         human_conversion: int
 ) -> None:
     assert get_human_age(cat_age, dog_age) == human_conversion
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age", [
+        (-1, 5),
+        (3, -50)
+    ]
+)
+def test_negative_age_raises_error(
+    cat_age: int,
+    dog_age: int
+) -> None:
+    with pytest.raises(ValueError, match="Age cannot be negative"):
+        get_human_age(cat_age, dog_age)
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age", [
+        ("10", 5),
+        (40, "8")
+    ]
+)
+def test_type_age_raises_error(
+    cat_age: int,
+    dog_age: int
+) -> None:
+    with pytest.raises(TypeError, match="Age must be a natural number"):
+        get_human_age(cat_age, dog_age)
